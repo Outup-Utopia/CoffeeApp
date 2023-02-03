f2c161e1be52b0625c8577b540efa5472218f527
aCt0802 <0802.kaz@gmail.com>
2023-02-03 20:44:37 +0900

    first commit

---
+++ app/src/main/java/dev/outup/coffeeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.outup.coffeeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
