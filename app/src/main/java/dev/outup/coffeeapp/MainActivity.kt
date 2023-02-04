739430688117b2845fbe13d2d161bfd271529d88
aCt0802 <0802.kaz@gmail.com>
2023-02-04 18:08:32 +0900

    ログイン画面と新規登録画面への遷移処理を追加

--- app/src/main/java/dev/outup/coffeeapp/MainActivity.kt
+++ app/src/main/java/dev/outup/coffeeapp/MainActivity.kt
@@ -1,11 +1,47 @@
 package dev.outup.coffeeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dev.outup.coffeeapp.view.LoginActivity
+import dev.outup.coffeeapp.view.RegisterUserActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    /**
+     * ログイン
+     * ログインせずに進む
+     * 新規登録
+     * A21DC217@dhw.ac.jp  pass:asyera221
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Initialize Firebase Auth
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loginButton: Button = findViewById(R.id.LoginBtn)
+        loginButton.setOnClickListener {
+            val intent = Intent(this.applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        val createAccountButton: Button = findViewById(R.id.CreateAccountBtn)
+        createAccountButton.setOnClickListener {
+            val intent = Intent(this.applicationContext, RegisterUserActivity::class.java)
+            startActivity(intent)
+        }
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val text: TextView = findViewById(R.id.SecondText)
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            text.text = "ログイン済みです"
+        } else {
+            text.text = "ログインしていません"
+        }
     }
 }
