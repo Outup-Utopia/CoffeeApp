0e9b1658e1fa3754447ff4952fbb372775e58fa3
aCt0802 <0802.kaz@gmail.com>
2023-02-06 15:14:26 +0900

    コーヒーのドメインモデル作成

---
+++ app/src/main/java/dev/outup/coffeeapp/domain/enums/CoffeeSize.kt
@@ -0,0 +1,4 @@
+package dev.outup.coffeeapp.domain.enums
+
+enum class CoffeeSize {
+}
---
+++ app/src/main/java/dev/outup/coffeeapp/domain/model/Coffee.kt
@@ -0,0 +1,4 @@
+package dev.outup.coffeeapp.domain.model
+
+class Coffee(val orderItem: String, val size: String, val options: List<Option>) : DomainModel {
+}
