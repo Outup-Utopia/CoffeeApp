7abe5717d5a78ced18d3b3e6b854685d3533f012
aCt0802 <0802.kaz@gmail.com>
2023-02-07 01:13:10 +0900

    [WIP]コンテンツ追加画面を作成

--- app/src/main/java/dev/outup/coffeeapp/view/AddContentsActivity.kt
+++ app/src/main/java/dev/outup/coffeeapp/view/AddContentsActivity.kt
@@ -1,15 +1,25 @@
 package dev.outup.coffeeapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import dev.outup.coffeeapp.R
-import dev.outup.coffeeapp.domain.model.Content
+import dev.outup.coffeeapp.domain.model.option.Appendable
+
 
 class AddContentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_contents)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        for (value in Appendable.values()) {
+            adapter.add(value.name)
+        }
+        val spinner: Spinner = findViewById<View>(R.id.optionNameSpinner) as Spinner
+        spinner.adapter = adapter
     }
 }
