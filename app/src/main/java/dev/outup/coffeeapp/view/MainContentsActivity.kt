f1475bda81ff2fa82e8c01cd742d43ec6144bd31
aCt0802 <0802.kaz@gmail.com>
2023-02-07 01:12:31 +0900

    main_contentsでもタイムラインが表示されるよう修正(最終的にはこちらにすべて寄せる)

--- app/src/main/java/dev/outup/coffeeapp/view/MainContentsActivity.kt
+++ app/src/main/java/dev/outup/coffeeapp/view/MainContentsActivity.kt
@@ -1,12 +1,46 @@
 package dev.outup.coffeeapp.view
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dev.outup.coffeeapp.MainActivity
 import dev.outup.coffeeapp.R
+import dev.outup.coffeeapp.domain.usecase.ContentService
+import dev.outup.coffeeapp.infrastructure.CustomAdapter
+import dev.outup.coffeeapp.infrastructure.repository.CoffeeRepositoryImpl
+import dev.outup.coffeeapp.infrastructure.repository.ContentRepositoryImpl
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 
 class MainContentsActivity : AppCompatActivity() {
+    private val auth: FirebaseAuth = Firebase.auth
+    private val contentService = ContentService(ContentRepositoryImpl, CoffeeRepositoryImpl)
+    private val scope = CoroutineScope(Job() + Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_contents)
+        setContentView(R.layout.activity_list_contents)
+
+        auth.currentUser ?: run {
+            val intent = Intent(this.applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val applicationContext = this.applicationContext
+        scope.async {
+            val contents = contentService.getTimeline(auth.currentUser!!.uid)
+            Log.d(ContentValues.TAG, "表示対象コンテンツ:")
+            Log.d(ContentValues.TAG, contents.toString())
+            val adapter = CustomAdapter(contents, applicationContext)
+            val recyclerView = findViewById<RecyclerView>(R.id.content_recycler)
+            recyclerView.adapter = adapter
+        }
     }
 }
