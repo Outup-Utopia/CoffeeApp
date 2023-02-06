5152e687b1b92216c9f8b6b0d6b748b2dec73697
aCt0802 <0802.kaz@gmail.com>
2023-02-07 01:09:09 +0900

    contentの手続きを作成(タイムライン取得のみ)

--- app/src/main/java/dev/outup/coffeeapp/domain/usecase/ContentService.kt
+++ app/src/main/java/dev/outup/coffeeapp/domain/usecase/ContentService.kt
@@ -1,6 +1,40 @@
 package dev.outup.coffeeapp.domain.usecase
 
+import dev.outup.coffeeapp.domain.model.Content
+import dev.outup.coffeeapp.domain.repository.CoffeeRepository
 import dev.outup.coffeeapp.domain.repository.ContentRepository
 
-class ContentService(val contentRepository: ContentRepository) {
+class ContentService(private val contentRepository: ContentRepository, private val coffeeRepository: CoffeeRepository) {
+    suspend fun getTimeline(userId: String): ArrayList<Content> {
+        val contents = contentRepository.loadAsTimeline(userId)
+        val contentModels: ArrayList<Content> = arrayListOf()
+        contents.forEach { content ->
+            if (content?.id != null) {
+                val coffee = coffeeRepository.loadById(content.id!!)
+                contentModels.add(
+                    Content(
+                        content.id,
+                        content.title,
+                        content.imageLocation,
+                        coffee,
+                        content.createdAt,
+                        content.updatedAt
+                    )
+                )
+            }
+        }
+        return contentModels
+    }
+
+    fun registerContent(userId: String, content: Content) {
+        TODO("implement")
+    }
+
+    fun updateContent(userId: String, content: Content) {
+        TODO("implement")
+    }
+
+    fun deleteContent(userId: String, documentId: String) {
+        TODO("implement")
+    }
 }
